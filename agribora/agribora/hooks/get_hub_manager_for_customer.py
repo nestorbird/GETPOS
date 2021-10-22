c462f0f3238a118ea70b983e5522d8697cbda42a
hemant.tripathi <hemant.tripathi@nestorbird.com>
2021-10-22 15:15:47 +0530

    Fix in hooks.py

--- agribora/agribora/hooks/get_hub_manager_for_customer.py
+++
@@ -1,6 +0,0 @@
-import frappe
-from frappe import _
-
-def get_hub_manager(doc,method):
-    hub_manager = frappe.get_value('Hub Manager',{'ward': doc.ward},'name')
-    doc.hub_manager = hub_manager
