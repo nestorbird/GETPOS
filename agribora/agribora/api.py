b6d019550e537fb21e8a871b73b324a93eaafda8
Habibur Rahman <habibur@nestorbird.com>
2021-10-28 08:58:23 +0000

    Replace api.py

--- agribora/agribora/api.py
+++ agribora/agribora/api.py
@@ -21,8 +21,9 @@ def login(usr, pwd):
             "success_key":0,
             "message":"Incorrect Username or Password"
         }
-
+        
         return
+
 
     api_generate = generate_keys(frappe.session.user)
     user = frappe.get_doc('User', frappe.session.user)
@@ -35,10 +36,9 @@ def login(usr, pwd):
         "api_secret":api_generate,
         "username":user.username,
         "email":user.email
-    }
-#     frappe.local.response[“type”] = "Redirect"
-#     frappe.local.response[“location”] = “/all-products”
-
+    } 
+        
+ 
 def generate_keys(user):
     user_details = frappe.get_doc('User', user)
     api_secret = frappe.generate_hash(length=15)
@@ -51,9 +51,10 @@ def generate_keys(user):
     user_details.save()
 
     return api_secret
+       
 
 # this is email going for the password reset
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 @rate_limit(key='user', limit=get_password_reset_limit, seconds = 24*60*60, methods=['POST'])
 def forgot_password(user):
         if user=="Administrator":
@@ -74,12 +75,12 @@ def forgot_password(user):
                 return 'not found'
         
 # this is for forgot-password message
-@frappe.whitelist( allow_guest=True )
+@frappe.whitelist()
 def reset_password( user,send_email=False, password_expired=False):
                 from frappe.utils import random_string, get_url
 
                 key = random_string(32)
-                # user.db_set("reset_password_key", key)
+                
 
                 url = "/update-password?key=" + key
                 if password_expired:
@@ -125,45 +126,23 @@ def get_abbr(string):
     abbr = ''.join(c[0] for c in string.split()).upper()
     return abbr
 
-# this is for customer list api
-@frappe.whitelist(allow_guest=True)
-def customer_list():
-        res = frappe.db.get_all('Customer')
-        return res
 
-# this is for customer detail api
-@frappe.whitelist(allow_guest=True)
-def customer_list_detail(name):
-        detail = frappe.db.get_value("Customer",name,["customer_name","email_id","mobile_no","ward","name","creation"],as_dict=1)
-        return detail   
+
+
 
 # this id for terms and conditions api
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def terms_and_conditions():
         term = frappe.db.get_value("Terms and Conditions","Terms and Conditions for agribora","terms")
         return term
 
 # this is for privacy policy api
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def privacy_policy():
         policy = frappe.db.get_value("Privacy Policy","Privacy Policy for agribora","privacy")
-        return policy 
+        return policy
 
-# this is for customer list belomg with hub manager
-@frappe.whitelist(allow_guest=True)
-def get_cust_belong_hubmngr():
-        cust = frappe.get_all("Customer")
-        hub = frappe.get_all("Hub Manager")
-        list = [ ]
-        for x in hub:
-                for y in cust:
-                        if x==y:
-                                list.append(x)
-        return list
-
-#this is for cust belong with hub manager detail
-@frappe.whitelist(allow_guest=True)
-def customer_list_belong_with_hub_detail(name):
-        detail = frappe.db.get_value("Customer",name,["customer_name","email_id","mobile_no","ward","name","creation"],as_dict=1)
-        return detail   
-
+#this is for customer list by hub manager
+@frappe.whitelist()
+def get_customer_list_by_hubmanager(hub_manager):
+              return frappe.db.get_list('Customer',{'hub_manager': hub_manager},["customer_name","email_id","mobile_no","ward","name","creation"])
