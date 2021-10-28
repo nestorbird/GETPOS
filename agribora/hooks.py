b8fd8feb76847fdcc7bde5371493b11221c4642f
abrarpv97 <abrarpv97@gmail.com>
2021-10-28 11:03:39 +0000

    create sales invoice, payment entry on sales order submission, T-9936
    (cherry picked from commit 7b51d7a995e233ff342d0d73bfa20decb6b32cee)

--- agribora/hooks.py
+++ agribora/hooks.py
@@ -33,7 +33,8 @@ app_license = "MIT"
 # include js in doctype views
 doctype_js = {
 	"Sales Order" : "public/js/doctype_js/sales_order.js",
-	"Warehouse" : "public/js/doctype_js/warehouse.js"
+	"Warehouse" : "public/js/doctype_js/warehouse.js",
+	"Account" : "public/js/doctype_js/account.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -95,6 +96,13 @@ doctype_js = {
 doc_events = {
 	"Warehouse": {
 		"validate": "agribora.agribora.hooks.warehouse.validate_hub_manager"
+	},
+	"Sales Order":{
+		"on_submit": "agribora.agribora.hooks.sales_order.on_submit",
+		"validate": "agribora.agribora.hooks.sales_order.validate"
+	},
+	"Sales Invoice":{
+		"on_submit": "agribora.agribora.hooks.sales_invoice.on_submit"
 	}
 }
 
