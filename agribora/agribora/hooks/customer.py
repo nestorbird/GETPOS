a152c44cc54382606e6f8bf49a49abc75ad1accf
Hemant Tripathi <hemant.tripathi@nestorbird.com>
2021-11-03 05:32:47 +0000

    Update agribora/agribora/hooks/customer.py

---
+++ agribora/agribora/hooks/customer.py
@@ -0,0 +1,6 @@
+import frappe
+
+def validate(doc,method):
+    if not doc.hub_manager:
+        hub_manager = frappe.db.get_value('Ward Detail', {'ward': doc.ward}, ['parent'])
+        doc.hub_manager = hub_manager
