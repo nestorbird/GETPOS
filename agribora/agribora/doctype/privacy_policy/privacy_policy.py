ab1c33399517b256f701e7901f40de41dae73db6
abrarpv97 <abrarpv97@gmail.com>
2021-11-03 14:13:27 +0000

    fix: api(login, hub manager details) and new doctype privacy policy

---
+++ agribora/agribora/doctype/privacy_policy/privacy_policy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, www.nestorbird.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class PrivacyPolicy(Document):
+	pass
