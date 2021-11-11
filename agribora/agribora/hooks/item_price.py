c0fe87062720ed9d5739e6869761520163355e4c
Sameeksha Kapse <sameekshakapse14@gmail.com>
2021-11-11 15:07:21 +0530

    deleted item price file

--- agribora/agribora/hooks/item_price.py
+++
@@ -1,7 +0,0 @@
-import frappe
-from frappe import _
-
-@frappe.whitelist(allow_guest=True)
-def validate_item_price(doc,method):
-    if doc.price_list_rate<=0:
-        frappe.throw(_("Rate can not be Zero or Negative"))
