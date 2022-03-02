4e240bc07a5f0201c5273e1a665156e53e4c324e
ashish.vashisht1 <ashish.vashisht@nestorbird.com>
2022-03-02 11:46:36 +0530

    fix: added item variant api

---
+++ nbpos/nbpos/doctype/item_group_multiselect/item_group_multiselect.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, swapnil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item Group Multiselect', {
+	// refresh: function(frm) {
+
+	// }
+});
