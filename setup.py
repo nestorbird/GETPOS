4f88c3e9acb9547e631d0a5fb68c7b254a1aef92
hemanttripathiNB <hemant.tripathi@nestorbird.com>
2024-02-08 17:35:23 +0530

    fix: module of nbpos

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in nbpos/__init__.py
+# get version from __version__ variable getpos/__init__.py
 from getpos import __version__ as version
 
 setup(
