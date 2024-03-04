b6692e47cf1cdf99e81f3c22efda15f8987f2f14
aashishvashisht09 <ashish.vashisht@nestorbird.com>
2024-03-04 13:27:16 +0000

    chore: remove unwanted code

--- getpos/hooks.py
+++ getpos/hooks.py
@@ -232,7 +232,4 @@ after_migrate = "getpos.getpos.after_migrate.main"
 
 
 
-website_route_rules = [
-    				{'from_route': '/GetPOS/<path:app_path>', 'to_route': 'GetPOS'},
-    				{'from_route': '/GetPOS', 'to_route': 'GetPOS'},
-                       ]
+website_route_rules = [{'from_route': '/GetPOS/<path:app_path>', 'to_route': 'GetPOS'},]
