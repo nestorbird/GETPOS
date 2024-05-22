40e7dee83ea792039e2ddd52d800a78508ef5fb6
Ajay <ajay.kumar@nestorbird.com>
2024-05-22 17:25:46 +0530

    Item Doctype Mappings With Cost Center and chnages in api

--- getpos/getpos/hooks/cost_center.py
+++ getpos/getpos/hooks/cost_center.py
@@ -5,3 +5,9 @@ def create_warehouse(self, method=None):
     new_warehouse.warehouse_name = self.cost_center_name
     new_warehouse.custom_cost_center = self.name
     new_warehouse.insert()
+
+
+@frappe.whitelist()
+def fetch_all_cost_centers():
+    cost_centers = frappe.get_all("Cost Center", fields=["name"])
+    return cost_centers
