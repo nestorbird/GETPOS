ea6942a5ee1875e9730733c756ffaf926cd8c173
ajay <ajay.kumar@nestorbird.com>
2024-06-26 18:29:25 +0530

    changes

--- getpos/getpos/hooks/customer.py
+++ getpos/getpos/hooks/customer.py
@@ -2,25 +2,6 @@ import frappe
 from frappe import _
 from frappe.model.naming import make_autoname
 
-def validate(doc,method):
-    if not doc.hub_manager:
-        hub_manager = frappe.db.get_value('Ward Detail', 
-                        {
-                            'ward': doc.ward, 
-                            'is_assigned': 1,
-                            'parenttype': 'Hub Manager'
-                        }, 
-                        ['parent'])
-        doc.hub_manager = hub_manager
-        
-
-def validate(doc,method):
-        doc.name = make_autoname("CUST"+"-.YYYY." +"-.###")
-        existing_customer = frappe.db.get_value("Customer", {"mobile_no": doc.mobile_no})
-        if existing_customer and doc.mobile_no is not None:
-            frappe.throw(_("Customer With Mobile Number {0} Already Exists").format(frappe.bold(doc.mobile_no)))
-
-
 
 @frappe.whitelist(allow_guest=True)
 def get_customer_by_ward(ward):
